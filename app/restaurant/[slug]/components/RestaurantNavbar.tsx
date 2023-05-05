43eee4ceac0cacf32e741ab9ef4e606c981fd73a
Michal <michal.jedro@wp.pl>
2023-05-05 13:46:54 +0200

    more refactor

--- app/restaurant/[slug]/components/RestaurantNavbar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavbar.tsx
@@ -3,12 +3,10 @@ export default function RestaurantNavbar() {
   return (
     <nav className="flex text-reg border-b pb-2">
       <Link href="/restaurant/tuti-santi/menu" className="mr-7">
-        {" "}
-        Overview{" "}
+        Overview
       </Link>
       <Link href="/restaurant/tuti-santi/menu" className="mr-7">
-        {" "}
-        Menu{" "}
+        Menu
       </Link>
     </nav>
   );
