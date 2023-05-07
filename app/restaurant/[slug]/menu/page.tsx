0ae8c62631c785838b239b7dc414180049f7aa90
Michał Jędro <michal.jedro@wp.pl>
2023-05-07 11:54:57 +0200

    create rootLayout with Navbar

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -11,17 +11,14 @@ import ReservationCard from "../components/ReservationCard";
 
 export default function RestaurantMenu() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Menu />
-          </div>
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+          <RestaurantNavbar />
+          <Menu />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 }
