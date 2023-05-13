90fb7cfea4735765ce92513508d10aeb08dac505
Michał Jędro <michal.jedro@wp.pl>
2023-05-13 18:06:45 +0200

    changes

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -9,20 +9,19 @@ import Header from "./components/Header";
 export default function RestaurantDetails() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
-
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[70%] rounded p-3 shadow">
+          <RestaurantNavbar />
+          <Title />
+          <Rating />
+          <Description />
+          <Images />
+          <Reviews />
         </div>
-      </main>
+        <div className="w-[27%] relative text-reg">
+          <ReservationCard />
+        </div>
+      </div>
+    </main>
   );
 }
